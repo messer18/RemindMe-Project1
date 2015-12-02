47dd23a0401d43040892ea5f5c4b730c064861de
messer18 <zota.viorel@mail.ru>
2015-12-02 18:45:34 +0200

    Create MainActivity annd layout activity_main

--- app/src/androidTest/java/main.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by messer on 02.12.2015.
- */
-public class main {
-    ///adsddsdsdsd
-}
---
+++ app/src/main/java/com/example/messer/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.messer.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by messer on 02.12.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
